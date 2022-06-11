55efe0a089c540bc2720e789e57d04d1e322bf68
Ruben Toro <rubensdt01@gmail.com>
2022-06-11 00:00:06 -0500

    Model of company

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+from turtle import update
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.models.CharField(max_length=100)
+    address = models.models.CharField(max_length=100)
+    phone = models.models.CharField(max_length=20)
+    nit = models.models.CharField(max_length=20)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- api/views.py
+++ api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def getRoutes(request):
+    
+    routes = [
+        {
+            'Endpoint': '/company/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Get all companies'
+        },
+                {
+            'Endpoint': '/company/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Get company by id'
+        },
+        {
+            'Endpoint': '/company/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Get all companies'
+        },
+        {
+            'Endpoint': '/company/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Update company info'
+        },
+        {
+            'Endpoint': '/company/id/delete/',
+            'method': 'DELETE',
+            'body': "",
+            'description': 'Delete company'
+        },
+    ]
+
+    return JsonResponse(routes, safe=False)
